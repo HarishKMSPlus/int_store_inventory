1654dd814fe1bd886ff16a54b3b133b88ed5af0b
Nehal.Shah <Nehal.Shah@SPT-CPU-0316.splus.local>
2018-03-06 19:03:25 +0530

    custom attributes values displayed

--- int_store_inventory/cartridge/controllers/Store.js
+++ int_store_inventory/cartridge/controllers/Store.js
@@ -1,29 +1,45 @@
 /**
  * Description of the Controller and the logic it provides
- *
- * @module  controllers/Store
+ * 
+ * @module controllers/Store
  */
+
 importPackage( dw.system );
 importPackage( dw.object );
 importPackage( dw.net );
+importPackage( dw.util );
+importPackage(dw.system);
 
 exports.Start = function(){
-	response.getWriter().println("hello");
-	var customURL= dw.system.Site.getCurrent().getCustomPreferenceValue('customURL');
-	response.getWriter().println("customURL : " +"   "+ customURL);
 
+	var customURL= dw.system.Site.getCurrent().getCustomPreferenceValue('customURL');
+
+	var allCustomPreferences = dw.system.Site.getCurrent().getCustomPreferenceValue('intStoreInventoryEnabled'); 
+
+	var userNamePreference  = dw.system.Site.getCurrent().getCustomPreferenceValue('userName'); 
+
+	var customPass = dw.system.Site.getCurrent().getCustomPreferenceValue('password'); 
+
+	response.getWriter().println("<h1></h1>");
 	var httpClient : HTTPClient = new HTTPClient();
 	httpClient.open( "GET",customURL);
 	httpClient.send();
-	response.getWriter().println("statusCode : " +"   "+ httpClient.statusCode);
-
+	// response.getWriter().println("statusCode : " +" "+ httpClient.statusCode);
+	response.getWriter().println("<h1></h1>");
 	if (httpClient.statusCode == 200) {
-
 		var message = httpClient.text; 
-		response.getWriter().println("message : " +"   "+ message);
-	
 	}else {
-		response.getWriter().println("message : " +"   "+ "Something went Wrong..");
+		message =  "Something went Wrong..";
 	}
+	var ISML = require('dw/template/ISML');
+	ISML.renderTemplate(
+			'storeDetails.isml', {
+				customURL: customURL,  
+				StoreInventoryEnabled : allCustomPreferences,
+				userNamePreference : userNamePreference,  
+				password : customPass,
+				message : message
+			}
+	);	
 };
 exports.Start.public = true; 
